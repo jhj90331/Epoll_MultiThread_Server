da900b76a38f1905099123a5dfaa6aa655d3e242
jhj90331 <37953786+jhj90331@users.noreply.github.com>
2021-04-16 16:08:25 +0900

    Create headers.h

---
+++ 21.04.16 Client/headers.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT 1818
+#include <iostream>
+#include <string>
+#include <sys/ioctl.h>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+#include <unordered_map>
+
+
+using namespace std;
