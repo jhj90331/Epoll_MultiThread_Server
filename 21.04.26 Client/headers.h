93eae9a5792395310e7c757acda3dc0cf0d1cfcb
jhj90331 <37953786+jhj90331@users.noreply.github.com>
2021-04-26 08:53:38 +0900

    Add files via upload

---
+++ 21.04.26 Client/headers.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT 1818
+#include <iostream>
+#include <string>
+#include <sys/ioctl.h>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+
+
+using namespace std;
---
+++ 21.04.26 Client/packet.h
@@ -0,0 +1,75 @@
+
+#ifndef PACKET_DEFINE_H_
+#define PACKET_DEFINE_H_
+
+#define MAX_DATA_SIZE 1024
+#define MAX_PRINT_DATA_SIZE 1024
+
+enum packetType
+{
+			EM_CMD_USER_CHAT_REQ = 100,
+			EM_CMD_USER_CHAT_RESULT,
+			EM_CMD_USER_LOG_REQ ,
+			EM_CMD_USER_LOG_RESULT,
+			EM_CMD_USER_LOG_DELETE_REQ,
+			EM_CMD_USER_LOG_DELETE_RESULT
+};
+#pragma pack(push, 1)	//1byte 정렬
+
+typedef struct PACKET_HEADER
+{
+	char head[5] = {"AA11"};
+	unsigned int datasize;
+}HEADER;
+
+typedef struct PACKET_BODY
+{
+	enum packetType cmd;
+	char data[MAX_DATA_SIZE+1];
+}BODY;
+
+typedef struct PACKET_TAIL
+{
+	char tail[5] = {"11AA"};
+}TAIL;
+
+typedef struct MILESTONE_PACKET
+{
+	HEADER phead;
+	BODY body;
+	TAIL ptail;
+}PACKET;
+
+#pragma pack(pop)	//정렬 설정을 이전 상태(기본값)로 되돌림
+
+
+#endif /* PACKET_DEFINE_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
